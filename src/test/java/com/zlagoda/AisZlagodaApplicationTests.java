e22b857cc832f4ee203f907e42028935c42dc1c2
Anton Ruban <antonruban03012005@gmail.com>
2024-06-07 13:12:30 +0300

    Initial commit

---
+++ src/test/java/com/zlagoda/AisZlagodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlagoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisZlagodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
