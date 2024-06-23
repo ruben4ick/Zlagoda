ffb6e80d5199eb1c3f38a0e298ccebce41e1a6b1
Illia Krachylo <kraciloi@gmail.com>
2024-06-23 13:29:07 +0300

    error page

--- src/main/java/com/zlagoda/config/MvcConfig.java
+++ src/main/java/com/zlagoda/config/MvcConfig.java
@@ -1,8 +1,10 @@
 package com.zlagoda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -11,4 +13,12 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
+
+//    @Bean(name = "simpleMappingExceptionResolver")
+//    public SimpleMappingExceptionResolver createSimpleMappingExceptionResolver() {
+//        SimpleMappingExceptionResolver r = new SimpleMappingExceptionResolver();
+//
+//        r.setDefaultErrorView("error");
+//        return r;
+//    }
 }
