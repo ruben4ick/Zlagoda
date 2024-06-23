872f89a16faa6d9707c26f5878d90573848dcde1
bbgdo <bohdan.tarverdiiev@gmail.com>
2024-06-23 14:35:08 +0300

    implemented select by date range for checks

--- src/main/java/com/zlagoda/service/CheckService.java
+++ src/main/java/com/zlagoda/service/CheckService.java
@@ -4,11 +4,13 @@ import com.zlagoda.dto.CheckDto;
 import com.zlagoda.entity.Check;
 
 import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CheckService extends GenericService<CheckDto, String> {
     List<CheckDto> getByEmplId(String id);
     List<CheckDto> getByEmplSurname(String surname);
-
+    List<CheckDto> selectByDate(List<CheckDto> checks, LocalDateTime start, LocalDateTime end);
     BigDecimal sumOfChecks(List<CheckDto> checks);
 }
