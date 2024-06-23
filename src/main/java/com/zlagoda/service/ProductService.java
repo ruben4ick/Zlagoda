3e947269c562506ac960a1f473addfba2b07fed7
Illia Krachylo <kraciloi@gmail.com>
2024-06-23 23:54:05 +0300

    21 and security tweaks

--- src/main/java/com/zlagoda/service/ProductService.java
+++ src/main/java/com/zlagoda/service/ProductService.java
@@ -3,11 +3,15 @@ package com.zlagoda.service;
 
 import com.zlagoda.dto.ProductDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ProductService extends GenericService<ProductDto, Long> {
     List<ProductDto> findByCategory(Long categoryId);
-
+    Optional<Integer> findTotalSalesByNameInDateRange(String productName,
+                                                         LocalDateTime startDate,
+                                                         LocalDateTime endDate);
     List<ProductDto> findByName(String name);
 }
